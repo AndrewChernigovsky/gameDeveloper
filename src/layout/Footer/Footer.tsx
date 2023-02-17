594f1392d68c4660521ffbe891480a0cfbf44479
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-02-18 00:50:16 +0600

    change icons color

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,47 +1,77 @@
-import {
-	Flex,
-	Grid,
-	Container,
-	Text
-} from '@chakra-ui/react';
+import { Flex, Grid, Container, Text } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
 
-import {Link} from "react-router-dom"
-import icons from './../../AllImages'
+import { Link } from "react-router-dom";
 
 interface IModeTheme {
-	colorMode: any;
-	toggleColorMode: () => void;
-	isDark: boolean;
+  colorMode: any;
+  toggleColorMode: () => void;
+  isDark: boolean;
 }
 
-const Footer = ({colorMode,
-toggleColorMode,
-isDark}: IModeTheme) => {
-	return (
-		<footer >
-			<Container maxW={{ lg: '1440px' }} fontFamily="Roboto" fontWeight={600} borderTop={`1px solid `}>
-				<Grid m="0 auto" alignContent="center" justifyContent="center" p={"20px 20px"} gap="20px">
-					<Text>Chat me: </Text>
-					<Flex mb="50px">
-						<Link to="https://vk.com/idcifrun" className='social-link'>
-							{icons.VkIcon()}
-						</Link>
-						<Link to="https://vk.com/idcifrun" className='social-link'>
-							{icons.TelegramIcon()}
-						</Link>
-						<Link to="https://vk.com/idcifrun" className='social-link'>
-							{icons.EmailIcon()}
+const Footer = ({colorMode, toggleColorMode }: IModeTheme) => {
+  const color = useColorModeValue("blue", "white");
 
-						</Link>
-
-					</Flex>
-					<Text>All rigths reserved 2023</Text>
-				</Grid>
-			
-				
-			</Container>
-		</footer>
-	);
+  return (
+    <footer>
+      <Container
+        maxW={{ lg: "1440px" }}
+        fontFamily="Roboto"
+        fontWeight={600}
+        borderTop={`1px solid `}
+      >
+        <Grid
+          m="0 auto"
+          alignContent="center"
+          justifyContent="center"
+          p={"20px 20px"}
+          gap="20px"
+        >
+          <Text>Chat me: </Text>
+          <Flex mb="50px">
+            <Link to="https://vk.com/idcifrun" className="social-link">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="65px"
+                height="65px"
+                fill={color}
+              >
+                <path d="M25,2C12.318,2,2,12.318,2,25s10.318,23,23,23c12.683,0,23-10.318,23-23S37.683,2,25,2z M34.973,29.535 c2.237,1.986,2.702,2.695,2.778,2.816C38.678,33.821,36.723,34,36.723,34h-4.12c0,0-1.003,0.011-1.86-0.557 c-1.397-0.915-2.86-2.689-3.888-2.381C25.992,31.32,26,32.486,26,33.483C26,33.84,25.693,34,25,34s-0.981,0-1.288,0 c-2.257,0-4.706-0.76-7.149-3.313c-3.456-3.609-6.487-10.879-6.487-10.879s-0.179-0.366,0.016-0.589 c0.221-0.25,0.822-0.218,0.822-0.218L14.909,19c0,0,0.376,0.071,0.646,0.261c0.223,0.156,0.347,0.454,0.347,0.454 s0.671,2.216,1.526,3.629c1.67,2.758,2.447,2.828,3.014,2.531C21.27,25.445,21,22.513,21,22.513s0.037-1.259-0.395-1.82 c-0.333-0.434-0.97-0.665-1.248-0.701c-0.225-0.029,0.151-0.423,0.63-0.648C20.627,19.059,21.498,18.986,23,19 c1.169,0.011,1.506,0.081,1.962,0.186C26.341,19.504,26,20.343,26,23.289c0,0.944-0.13,2.271,0.582,2.711 c0.307,0.19,1.359,0.422,3.231-2.618c0.889-1.442,1.596-3.834,1.596-3.834s0.146-0.263,0.373-0.393 c0.232-0.133,0.225-0.13,0.543-0.13S35.832,19,36.532,19c0.699,0,1.355-0.008,1.468,0.402c0.162,0.589-0.516,2.607-2.234,4.797 C32.943,27.793,32.63,27.457,34.973,29.535z" />
+              </svg>
+            </Link>
+            <Link to="https://vk.com/idcifrun" className="social-link">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 100 100"
+                width="120px"
+                height="125px"
+                baseProfile="basic"
+                fill={color}
+              >
+                <path d="M36,12c13.255,0,24,10.745,24,24S49.255,60,36,60S12,49.255,12,36S22.745,12,36,12z M44.279,45.783	c0.441-1.354,2.51-14.853,2.765-17.513c0.077-0.806-0.177-1.341-0.676-1.58c-0.603-0.29-1.496-0.145-2.533,0.229	c-1.421,0.512-19.59,8.227-20.64,8.673c-0.995,0.423-1.937,0.884-1.937,1.552c0,0.47,0.279,0.734,1.047,1.008	c0.799,0.285,2.812,0.895,4.001,1.223c1.145,0.316,2.448,0.042,3.178-0.412c0.774-0.481,9.71-6.46,10.351-6.984	c0.641-0.524,1.152,0.147,0.628,0.672c-0.524,0.524-6.657,6.477-7.466,7.301c-0.982,1.001-0.285,2.038,0.374,2.453	c0.752,0.474,6.163,4.103,6.978,4.685c0.815,0.582,1.641,0.846,2.398,0.846S43.902,46.94,44.279,45.783z" />
+              </svg>
+            </Link>
+            <Link to="https://vk.com/idcifrun" className="social-link">
+              <svg
+                id="Layer_1"
+                viewBox="0 0 500 500"
+                width="50px"
+                height="50px"
+                fill={color}
+              >
+                <g>
+                  <path d="M67,148.7c11,5.8,163.8,89.1,169.5,92.1c5.7,3,11.5,4.4,20.5,4.4c9,0,14.8-1.4,20.5-4.4c5.7-3,158.5-86.3,169.5-92.1   c4.1-2.1,11-5.9,12.5-10.2c2.6-7.6-0.2-10.5-11.3-10.5H257H65.8c-11.1,0-13.9,3-11.3,10.5C56,142.9,62.9,146.6,67,148.7z" />
+                  <path d="M455.7,153.2c-8.2,4.2-81.8,56.6-130.5,88.1l82.2,92.5c2,2,2.9,4.4,1.8,5.6c-1.2,1.1-3.8,0.5-5.9-1.4l-98.6-83.2   c-14.9,9.6-25.4,16.2-27.2,17.2c-7.7,3.9-13.1,4.4-20.5,4.4c-7.4,0-12.8-0.5-20.5-4.4c-1.9-1-12.3-7.6-27.2-17.2l-98.6,83.2   c-2,2-4.7,2.6-5.9,1.4c-1.2-1.1-0.3-3.6,1.7-5.6l82.1-92.5c-48.7-31.5-123.1-83.9-131.3-88.1c-8.8-4.5-9.3,0.8-9.3,4.9   c0,4.1,0,205,0,205c0,9.3,13.7,20.9,23.5,20.9H257h185.5c9.8,0,21.5-11.7,21.5-20.9c0,0,0-201,0-205   C464,153.9,464.6,148.7,455.7,153.2z" />
+                </g>
+              </svg>
+            </Link>
+          </Flex>
+          <Text>All rigths reserved 2023</Text>
+        </Grid>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
