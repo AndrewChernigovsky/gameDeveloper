ea54b2a4e741b05f228f4bd41ecb0ceb75e0f866
MaximProsin <maximprosin647@gmail.com>
2023-02-18 22:44:29 +0300

    skills section first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
 import Header from "./layout/Header/Header";
 import HomePage from "./pages/HomePage/Homepage";
 import Skills from "./Content/Skills/Skills";
-=======
-import Header from './layout/Header/Header';
-import HomePage from './pages/HomePage/Homepage';
-import Footer from './layout/Footer/Footer';
-import './App.css';
-import { useColorMode } from '@chakra-ui/react';
->>>>>>> 9066a2166097aa8745e09a372626281de2bd0923
+import Footer from "./layout/Footer/Footer";
+
+import { useColorMode } from "@chakra-ui/react";
 
 function App() {
 	const { colorMode, toggleColorMode } = useColorMode();
 	const isDark = colorMode === 'dark';
 
 	return (
-<<<<<<< HEAD
 		<div>
 			<Header/>
 			<Skills/>
-=======
-		<div className="wrapper">
-			<Header
-				colorMode={colorMode}
-				toggleColorMode={toggleColorMode}
-				isDark={isDark}
-			/>
->>>>>>> 9066a2166097aa8745e09a372626281de2bd0923
 			<HomePage />
 			<Footer
 				colorMode={colorMode}
