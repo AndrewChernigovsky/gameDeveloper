7a21db230b99b7143bc8ca5a05f48079c6b0c267
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-02-20 00:58:16 +0600

    dark theme fixes and about us block

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { Flex, Container, Switch, IconButton } from '@chakra-ui/react';
 
 import { HashLink } from 'react-router-hash-link';
-
 import { useState } from 'react';
+
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { useColorModeValue } from '@chakra-ui/react';
+import { Darktheme } from '../theme';
 
 
 interface IModeTheme {
@@ -14,9 +15,9 @@ interface IModeTheme {
 
 const Header = ({ toggleColorMode, isDark }: IModeTheme) => {
 	const [display, changeDisplay] = useState('none');
-	const color = useColorModeValue('black', 'white');
+	const color = useColorModeValue('#000080', 'white');
 	const colorFonts = useColorModeValue('white', 'black');
-	const colorSwitch = useColorModeValue('blue', 'white');
+	const colorSwitch = useColorModeValue('blue', 'blue');
 
 	return (
 		<header style={{ backgroundColor: color, minHeight: '70px', position: 'fixed', minWidth: '100%', borderBottom: `1px solid ${color}`, paddingBottom: '20px', color: colorFonts,}}>
@@ -28,15 +29,6 @@ const Header = ({ toggleColorMode, isDark }: IModeTheme) => {
 						right="1rem"
 						align="center"
 					></Flex>
-					<Switch
-						pos="fixed"
-						top="20px"
-						right="10px"
-						colorScheme={colorSwitch}
-						size="lg"
-						isChecked={isDark}
-						onChange={toggleColorMode}
-					/>
 				</Flex>
 				<Flex
 					w="100vw"
--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -1,8 +1,12 @@
+import About from "../../Content/About/About";
+import Intro from "../../Content/Intro/Intro";
 import Portfolio from "../../Content/Portfolio/Portfolio";
 
 const HomePage = () => {
 	return (
 		<main className="main">
+			<Intro />
+			<About />
 			<Portfolio/>
 		</main>
 	);
