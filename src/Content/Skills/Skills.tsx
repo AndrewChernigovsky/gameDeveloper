8ba3e8e17ae9f98b58fce701f2509726cf265820
MaximProsin <maximprosin647@gmail.com>
2023-02-19 22:51:57 +0300

    skills section adaptive (prototip) comlete

--- src/Content/Skills/Skills.tsx
+++ src/Content/Skills/Skills.tsx
@@ -18,18 +18,18 @@ const Skills = () => {
                     minWidth='100%'
                 >
                     <Heading as="h2" size="4xl" mb="30px" textAlign="center" textTransform="uppercase">Skills</Heading>
-                    <Flex minWidth='100%' align='center' display='flex' alignItems='flex-start' justifyContent={{ base: 'center', md: 'center', lg: 'space-between' }} flexDirection={{ base: 'column', md: 'column', lg: 'row' }} gap='20px'>
-                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} h='auto' bg={skillsBg} color={skillsColor} padding='20px'>
-                            <Text align='center' fontSize='20px' height='50px'>Expert knowledge in the field of programming</Text>
-                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center' minH={{ base: '70px', md: '70px', lg: '100px' }}>Using unreal engine 4/5 C++ programming knowledge and drawings</Text>
+                    <Flex minWidth='100%' align='center' display='flex' alignItems='flex-start' justifyContent={{ base: 'center', sm1: 'space-between' }} flexDirection={{ base: 'column',sm1: 'row', lg: 'row' }} gap='20px'>
+                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' bg={skillsBg} color={skillsColor} padding='20px'>
+                            <Heading textAlign='center' fontSize='20px'>Expert knowledge in the field of programming</Heading>
+                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center'>Using unreal engine 4/5 C++ programming knowledge and drawings</Text>
                         </Box>
-                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} h='auto' bg={skillsBg} color={skillsColor} padding='20px'>
-                            <Text align='center' fontSize='20px' height='50px'>Level Design</Text>
-                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center' minH={{ base: '70px', md: '70px', lg: '100px' }}>Using the latest technology to create levels</Text>
+                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' bg={skillsBg} color={skillsColor} padding='20px'>
+                            <Heading textAlign='center' fontSize='20px'>Level Design</Heading>
+                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center'>Using the latest technology to create levels</Text>
                         </Box>
-                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} h='auto' bg={skillsBg} color={skillsColor} padding='20px'>
-                            <Text align='center' fontSize='20px' height='50px'>Architectural visualization</Text>
-                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center' minH={{ base: '70px', md: '70px', lg: '100px' }}>Creating cinematic and photorealistic videos and photos</Text>
+                        <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' bg={skillsBg} color={skillsColor} padding='20px'>
+                            <Heading textAlign='center' fontSize='20px'>Architectural visualization</Heading>
+                            <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center'>Creating cinematic and photorealistic videos and photos</Text>
                         </Box>
                     </Flex>
                 </Box>
--- src/layout/theme.ts
+++ src/layout/theme.ts
@@ -4,6 +4,7 @@ const fonts = { roboto: `'Roboto', sans-serif` }
 
 const breakpoints = {
   sm: '320px',
+  sm1: '600px',
   md: '768px',
   lg: '960px',
   xl: '1200px',
