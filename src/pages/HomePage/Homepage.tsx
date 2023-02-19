3f1259724ed7388dad5f7780c7baa48c60902fa1
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-02-19 22:14:29 +0300

    1

--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -1,5 +1,6 @@
 import Portfolio from "./../../Content/Portfolio/Portfolio";
 import Advantages from "./../../Content/Advantages/Advantages"
+import Intro from './../../Content/Intro/Intro';
 
 interface IModeTheme {
 	toggleColorMode: () => void;
@@ -8,6 +9,7 @@ interface IModeTheme {
 const HomePage = ({ toggleColorMode }: IModeTheme) => {
 	return (
 		<main className="main">
+			<Intro />
 			<Advantages toggleColorMode={toggleColorMode} />
 			<Portfolio />
 
