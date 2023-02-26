fbe5772849a33177e95e22f68d6f76ca3e5f6517
MaximProsin <maximprosin647@gmail.com>
2023-02-26 17:55:43 +0300

    skills finally

--- src/Content/Skills/Skills.tsx
+++ src/Content/Skills/Skills.tsx
@@ -21,21 +21,21 @@ const Skills = () => {
                     <Heading as="h2" size="4xl" mb="30px" textAlign="center" textTransform="uppercase">Skills</Heading>
                     <Flex minWidth='100%' align='center' display='flex' alignItems='flex-start' justifyContent={{ base: 'center', sm1: 'space-between' }} flexDirection={{ base: 'column', sm1: 'row', lg: 'row' }} gap='20px'>
                         <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' color={skillsColor} padding='20px' alignContent="flex-start">
-                            <Box margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' width="300px" height="300px">
+                            <Box className='icon-wrapper' margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' w="auto" minH="auto">
                                 <Knowledge />
                             </Box>
                             <Heading textAlign='center' fontSize='20px'>Expert knowledge in the field of programming</Heading>
                             <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center'>Using unreal engine 4/5 C++ programming knowledge and drawings</Text>
                         </Box>
                         <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' color={skillsColor} padding='20px' alignContent="flex-start">
-                            <Box margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' width="300px" height="300px">
+                            <Box className='icon-wrapper' margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' w="auto" minH="auto">
                                 <Design />
                             </Box>
                             <Heading textAlign='center' fontSize='20px'>Level Design</Heading>
                             <Text fontSize='16' opacity='70%' paddingTop={{ base: '10px', md: '20px', lg: '30px' }} align='center'>Using the latest technology to create levels</Text>
                         </Box>
                         <Box w={{ base: '100%', md: '100%', lg: '30%' }} minH={{ base: '70px', sm1: '300px', lg: '200px' }} display="flex" flexDirection='column' justifyContent='center' color={skillsColor} padding='20px' alignContent="flex-start">
-                            <Box margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' width="300px" height="300px">
+                            <Box className='icon-wrapper' margin="0 auto" display="flex" justifyContent="center" alignItems="center" borderRadius="50%" bgColor='transparent' border="2px solid white" padding='20px' mb='10px' w="auto" minH="auto">
                                 <Prism />
                             </Box>
                             <Heading textAlign='center' fontSize='20px'>Architectural visualization</Heading>
